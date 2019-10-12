101183ce20e0d0a02e5884dab8952f875234fdf0
Matt <mates097@seznam.cz>
2019-10-12 23:18:37 +0200

    Updated to work with config; made all commands lowercased

--- solo.js
+++ solo.js
@@ -1,4 +1,7 @@
 const Discord = require('discord.js');
+const config = require('./botconfig.json'); //Reads config (prefix, maybe owner in the future, etc.) from external file
+
+const prefix = config.prefix; //Reads the prefix from botconfig.json
 var Solo = new Discord.Client({
     disableEveryone: true
 });
@@ -12,8 +15,22 @@ Solo.on("message", async message => {
     if (message.author.bot) return;
     if (message.channel.type === "dm") return;
     let msgArray = message.content.split(' ');
-    let cmd = msgArray[0]; //CMD
+    
+    let cmd = msgArray[0].toLowerCase(); //CMD
     let arg = msgArray.slice(1); //All the args after the Command as an Array
+
+    if(cmd === `${prefix}ping`)
+    {
+        let ping = Math.round(Solo.ping);
+        return message.channel.send(`${ping} ms`);
+    }
 });
+
+
+
+function debugLog(commandName, user)
+{
+    console.log(`Command ${commandName} was used by ${user}`);
+}
 
 Solo.login('NDcxNDE0NjY1MzA2NzAxODQ0.XaIp3w.cIXfAjyk-WXpdift8Jpu92kYIVU');
