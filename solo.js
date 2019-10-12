f12daff2d858dbcf655cdafe4471f15eec535c7e
gitflee <fleeprivate@gmail.com>
2019-10-12 22:02:59 +0200

    ready to be worked on

--- solo.js
+++ solo.js
@@ -1,12 +1,19 @@
 const Discord = require('discord.js');
-var Solo = new Discord.Client({disableEveryone: true});
+var Solo = new Discord.Client({
+    disableEveryone: true
+});
+const fs = require('fs');
 
 Solo.on("ready", async () => {
     console.log('Logged in.');
 });
 
-Solo.on("message", async () => {
-    
+Solo.on("message", async message => {
+    if (message.author.bot) return;
+    if (message.channel.type === "dm") return;
+    let msgArray = message.content.split(' ');
+    let cmd = msgArray[0]; //CMD
+    let arg = msgArray.slice(1); //All the args after the Command as an Array
 });
 
 Solo.login('NDcxNDE0NjY1MzA2NzAxODQ0.XaIp3w.cIXfAjyk-WXpdift8Jpu92kYIVU');
