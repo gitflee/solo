9fe1ddae6fa696e6b72838bb25700ec98b3030c1
gitflee <fleeprivate@gmail.com>
2019-10-12 21:39:54 +0200

    Commit Suicide amirite xd

--- solo.js
+++ solo.js
@@ -2,7 +2,11 @@ const Discord = require('discord.js');
 var Solo = new Discord.Client({disableEveryone: true});
 
 Solo.on("ready", async () => {
-    console.log('Logged in as ${Solo.user.tag}');
+    console.log('Logged in.');
+});
+
+Solo.on("message", async () => {
+    
 });
 
 Solo.login('NDcxNDE0NjY1MzA2NzAxODQ0.XaIp3w.cIXfAjyk-WXpdift8Jpu92kYIVU');
